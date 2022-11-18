94d38995a5628980162d87874faeab910925c9d4
robertferry <robertmferry@gmail.com>
2022-11-17 21:32:14 -0600

    almost got it

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-const sections = document.querySelectorAll('.section');
+const sections = document.querySelectorAll('.main-content');
+const sect = document.querySelectorAll('.section');
+
 const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelectorAll('.main-content');
-
 
 function IconTransition() {
-    for(let i = 0; i < sectBtn.length; i++){
-        sectBtn[i].addEventListener('click', function() {
-            let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += ' active-btn';
+    //!Button click active
+        document.addEventListener('click', function (e) {
+          if (e.target.matches('.control')) {
+            document.querySelector('.active-btn').classList.remove('active-btn')
+            e.target.classList.add('active-btn')
+            document.querySelector('.active').classList.remove('active')
+            document.getElementById(e.target.dataset.id).classList.add('active')
+            return
+          }
+          if (e.target.matches('.theme-btn')) {
+            document.body.classList.toggle('light-mode')
+            return
+          }
         })
-    }
-   
-}
+      } 
 IconTransition();
+//!Everything above is going to be for operation of single page JS.
+
+
+
